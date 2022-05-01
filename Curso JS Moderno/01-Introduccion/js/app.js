b8f96072eab756026cedadaa9463c10d0824b8ea
Alexis David Pizarro Abarca <PZadpizarro@ads.loc>
2022-04-30 21:02:29 -0600

    comments

--- Curso JS Moderno/01-Introduccion/js/app.js
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -1,3 +1,7 @@
+//Pregunta cual es el nombre y lo almacena en la variable
 const nombre = prompt('Cual es tu nombre?');
+//Muestra el nombre almacenado en el .contenido del html
 document.querySelector('.contenido').innerHTML=`${nombre} esta aprendiendo JavaScript Moderno`;
+//Almacena en la variable producto el valor
+const producto = 'Monitor de 24 pulgadas'
 
